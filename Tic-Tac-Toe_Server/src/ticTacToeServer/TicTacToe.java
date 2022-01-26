759887ed055b7e4e3f0650558a2dc4b86c66e27b
Ahmed ElKhouly <ahmed.elsayed.elkhouly99@gmail.com>
2022-01-26 20:26:04 +0200

    update master

--- Tic-Tac-Toe_Server/src/ticTacToeServer/TicTacToe.java
+++ Tic-Tac-Toe_Server/src/ticTacToeServer/TicTacToe.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tic.tac.toe;
+package ticTacToeServer;
 
 import java.io.DataInputStream;
 import java.io.IOException;
